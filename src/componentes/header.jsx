1ef9e1b017a07aaeb3770fd7d43722587d8a7594
TomasOspina11 <tomasyosefospina11@gmail.com>
2026-08-11 07:05:25 -0500

    Estructura de componetes y organizacion de estilos css

---
+++ src/componentes/header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header-container">
+      <h1>Leidy Solangie Bernal Plazas</h1>
+      <h2>Desarrolladora Software</h2>
+    </header>
+  );
+}
